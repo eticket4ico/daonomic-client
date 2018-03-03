2c986429d34ef493ca04ce9ac926d2b8d11b5ba1
0v1se <dev@0v1se.com>
2018-03-03 17:54:30 +0300

    fix et4 configs

--- source/config/common.js
+++ source/config/common.js
@@ -1,11 +1,11 @@
 // @flow
 
 const sales = {
-  development: '0x938a97375d6809f7fcd191139e6658d6a5109a48',
+  development: '0xaee0eb707a23fee852c43b5eb22030a25a729937',
   production: '0x99a09f0d85bc6e95e110348a8522f98443e31c4a',
 };
 
-export const realm = '5a7f5cf23f6b6b92b6fdd68a';
+export const realm = '5a55f593fe725c0db41d1f43';
 export const sale: string = sales[process.env.API] || sales.production;
 export const contactEmail = 'dev@0v1se.com';
 export const termsOfServiceURL = 'terms.url';
