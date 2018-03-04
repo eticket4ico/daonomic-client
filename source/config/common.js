f80c7b071c38d1c5b23c8c774976c4d415c8964a
Dev <dev@0v1se.com>
2018-03-05 00:02:50 +0300

    PublicSale deployed to prod

--- source/config/common.js
+++ source/config/common.js
@@ -2,10 +2,10 @@
 
 const sales = {
   development: '0xaee0eb707a23fee852c43b5eb22030a25a729937',
-  production: '0x99a09f0d85bc6e95e110348a8522f98443e31c4a',
+  production: '0xf76350f0ad6b7cfce1311ef43e2eef488fd16dad',
 };
 
-export const realm = '5a55f593fe725c0db41d1f43';
+export const realm = '5a6f813d1d20a7d7c95eacb0';
 export const sale: string = sales[process.env.API] || sales.production;
 export const contactEmail = 'dev@0v1se.com';
 export const termsOfServiceURL = 'terms.url';
